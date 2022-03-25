f95d3d993da709282bc9a7abc30cdcdeb5140c9f
CB <wtbatson@gmail.com>
2022-03-25 10:43:44 -0500

    logging req urls

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
+let urlLogger = (req, res, next) => {
+    let url = req.url;
+    console.log(log);
+    fs.appendFile("request_logs.txt", log + "\n", err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    next();
+}
 let app = express();
 
-app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+// app.get('/', (req, res, next) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'))
 
-});
+// });
 
-app.get('/css/styles.css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/css/styles.css'));
-});
+// app.get('/css/styles.css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/css/styles.css'));
+// });
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(urlLogger);
 
 app.listen(3000);
