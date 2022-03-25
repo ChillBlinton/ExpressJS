caf18796a5cacbf32ecadf2132caf5d23c93af09
CB <wtbatson@gmail.com>
2022-03-25 10:28:03 -0500

    setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+let app = express();
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+
+});
+
+app.get('/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/css/styles.css'));
+});
+
+app.listen(3000);
